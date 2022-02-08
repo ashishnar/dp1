7a82bb145483bb35f5858b46d555d5576145d51d
ashishnar <94973798+ashishnar@users.noreply.github.com>
2022-02-08 17:13:06 +0530

    Add files via upload

--- berss_ers_zen.py
+++ berss_ers_zen.py
@@ -5,12 +5,12 @@
 import nuflux
 import numpy as np
 import matplotlib.pyplot as plt
-flux1 = nuflux.makeFlux('bartol')
-flux2 = nuflux.makeFlux('honda2006')
+flux1 = nuflux.makeFlux('BERSS_H3p_central')
+flux2 = nuflux.makeFlux('sarcevic_std')
 nu_type1=nuflux.NuMu
 nu_type2=nuflux.NuE
 nu_energy1 =100
-nu_energy2 =10
+nu_energy2 =1000
 x = np.arange(start=0, stop=100, step=1)
 nu_cos_zenith= -1+x*0.02
 fluxmu1=flux1.getFlux(nu_type1,nu_energy1,nu_cos_zenith)
@@ -28,6 +28,9 @@ plt.plot(nu_cos_zenith,fratio3,color='red', linestyle='dashed', linewidth = 3)
 fluxmu4=flux2.getFlux(nu_type1,nu_energy2,nu_cos_zenith)
 fluxe4=flux2.getFlux(nu_type2,nu_energy2,nu_cos_zenith)
 fratio4 = fluxmu4/fluxe4
+print(fluxe4)
+print(fluxmu4)
+print(fratio4)
 plt.plot(nu_cos_zenith,fratio4,color='blue', linestyle='dashed', linewidth = 3)
 plt.xlabel(r'$cos(\theta)$',fontsize = 20)
 # naming the y axis
@@ -41,7 +44,7 @@ plt.yticks(fontsize=20)
 # plt.xscale("log")
 # plt.yscale("log")
 # plt.title(r'Flux versus $cos(\theta)$ for bartol and honda2006(E=100 GeV)',fontsize = 20)
-plt.legend(["bartol at 100 GeV", "honda group at 100 GeV","bartol group at 10 GeV", "honda group 10 GeV"], loc ="upper center",fontsize = 20)
+plt.legend(["BERSS at 100 GeV", "BERSS group at 100 GeV","ERS at 10 GeV", "ERS 10 GeV"], loc ="upper center",fontsize = 20)
 plt.show()
 
 # plt.plot(nu_energy,(fluxmu1/fluxmu2),color='blue', linestyle='solid', linewidth = 3)
